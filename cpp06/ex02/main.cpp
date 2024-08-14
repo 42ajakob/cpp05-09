9326fcbe7fd5a3b21e2321457c79fe36d198fdab
42ajakob <ajakob@student.42heilbronn.de>
2024-08-14 06:12:16 +0200

    finished

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -31,12 +31,14 @@ void identify(Base* p) {
 		std::cout << "B\n";
 	else if (dynamic_cast<C*>(p))
 		std::cout << "C\n";
+	else if (p == NULL)
+		std::cout << "is NULL!";
 }
 
 void identify(Base& p) {
 	try {
 		(void)dynamic_cast<A&>(p);
-		std::cout << "A" << std::endl;
+		std::cout << "Pointer: A" << std::endl;
 		return ;
 	}
 	catch (const std::exception &e) {
@@ -44,7 +46,7 @@ void identify(Base& p) {
 
 	try {
 		(void)dynamic_cast<B&>(p);
-		std::cout << "B" << std::endl;
+		std::cout << "Pointer: B" << std::endl;
 		return ;
 	}
 	catch (const std::exception &e) {
@@ -52,7 +54,7 @@ void identify(Base& p) {
 
 	try {
 		(void)dynamic_cast<C&>(p);
-		std::cout << "C" << std::endl;
+		std::cout << "Pointer: C" << std::endl;
 		return ;
 	}
 	catch (const std::exception &e) {
@@ -61,9 +63,6 @@ void identify(Base& p) {
 
 int main() {
 	Base* ptr = generate();
-	
-	if (ptr == NULL)
-		return 1;
 
 	identify(ptr);
 	identify(*ptr);
